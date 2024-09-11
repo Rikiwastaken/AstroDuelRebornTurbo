972a56651883eea23efd0a05ef96af73e0874cf3
krafft5u <krafft5u@etu.univ-lorraine.fr>
2024-09-11 09:28:40 +0200

    ajout dash

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -14,9 +14,13 @@ public class movement : MonoBehaviour
 
     public InputActionReference cam;
 
+    public InputActionReference dash;
+
     private Vector2 movementinput;
 
     private Vector2 caminput;
+
+    private bool dashinput;
 
     public float maxspeed;
 
@@ -26,6 +30,12 @@ public class movement : MonoBehaviour
 
     private float averagespeed;
 
+    public float dashstr;
+
+    private bool dashed;
+
+    
+
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +43,8 @@ public class movement : MonoBehaviour
         move.action.performed += OnMovementChange;
 
         cam.action.performed += OnCamChange;
+
+        dash.action.performed += OnDashChange;
 
         RB2D = GetComponent<Rigidbody2D>();
     }
@@ -50,7 +62,15 @@ public class movement : MonoBehaviour
             caminput = Vector2.zero;
         }
 
-        if(caminput!=Vector2.zero)
+        if (!dash.action.IsPressed())
+        {
+            dashinput = false;
+            dashed = false;
+        }
+
+        
+
+        if (caminput!=Vector2.zero)
         {
             float angleA = 0f;
             if (Mathf.Atan2(caminput.x, caminput.y) * Mathf.Rad2Deg != 0)
@@ -58,8 +78,10 @@ public class movement : MonoBehaviour
                 angleA = Mathf.Atan2(caminput.x, -caminput.y) * Mathf.Rad2Deg;
                 transform.eulerAngles = new Vector3(0f, 0f, angleA + 90);
             }
+            Dashfct(caminput);
+
         }
-        else
+        else if(movementinput!=Vector2.zero)
         {
             float angleA = 0f;
             if (Mathf.Atan2(movementinput.x, movementinput.y) * Mathf.Rad2Deg != 0)
@@ -67,6 +89,11 @@ public class movement : MonoBehaviour
                 angleA = Mathf.Atan2(movementinput.x, -movementinput.y) * Mathf.Rad2Deg;
                 transform.eulerAngles = new Vector3(0f, 0f, angleA + 90);
             }
+            Dashfct(movementinput);
+        }
+        else
+        {
+            Dashfct(new Vector2(-transform.right.x,transform.right.y));
         }
 
         averagespeed = (float)(Mathf.Abs(RB2D.velocity.x) + Mathf.Abs(RB2D.velocity.y))/2f;
@@ -93,6 +120,28 @@ public class movement : MonoBehaviour
     public void OnCamChange(InputAction.CallbackContext context)
     {
         caminput = context.ReadValue<Vector2>();
+    }
+
+    public void OnDashChange(InputAction.CallbackContext context)
+    {
+        if(context.ReadValue<float>()!=0f)
+        {
+            dashinput = true;
+        }
+        else
+        {
+            dashinput = false;
+        }
+    }
+
+    public void Dashfct(Vector2 dir)
+    {
+        if (dashinput && !dashed)
+        {
+            RB2D.velocity=Vector2.zero;
+            dashed = true;
+            RB2D.AddForce(dir * dashstr);
+        }
     }
 
 }
